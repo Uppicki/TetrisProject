a239a6e14645655ec9ab55e2b6ce1b4d4c39a56e
vovav <vladimiruppicki@gmail.com>
2022-07-08 19:43:46 +0300

    Project was created with bugs

--- app/src/main/java/com/example/tetrisproject/GameActivity.kt
+++ app/src/main/java/com/example/tetrisproject/GameActivity.kt
@@ -1,29 +1,44 @@
 package com.example.tetrisproject
 
+import android.annotation.SuppressLint
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MotionEvent
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import com.example.tetrisproject.models.AppModel
 import com.example.tetrisproject.storage.AppPreferences
+import com.example.tetrisproject.view.TetrisView
 
 class GameActivity : AppCompatActivity() {
 
     var tvHighScore: TextView? = null
     var tvCurrentScore: TextView? = null
-    var appPreferences: AppPreferences? = null
 
+    private lateinit var tetrisView: TetrisView
+    var appPreferences: AppPreferences? = null
+    private val appModel: AppModel = AppModel()
+
+    @SuppressLint("ClickableViewAccessibility")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_game)
         appPreferences = AppPreferences(this)
+        appModel.setPreferences(appPreferences)
+
 
         val btnRestart = findViewById<Button>(R.id.btn_restart)
         val btnBackToStart = findViewById<Button>(R.id.btn_back_to_start)
         tvHighScore = findViewById<TextView>(R.id.tv_high_score)
         tvCurrentScore = findViewById<TextView>(R.id.tv_current_score)
+        tetrisView = findViewById<TetrisView>(R.id.view_tetris)
+        tetrisView.setActivity(this)
+        tetrisView.setModel(appModel)
 
+        tetrisView.setOnTouchListener(this::onTetrisViewTouch)
+        btnRestart.setOnClickListener(this::onRestartClick)
         btnBackToStart.setOnClickListener(this::onBackToStartClick)
 
         updateHighScore()
@@ -43,5 +58,40 @@ class GameActivity : AppCompatActivity() {
         tvHighScore?.text = "${appPreferences?.getHighScore()}"
     }
 
-    private fun onRestartClick(view: View) {}
+    private fun onRestartClick(view: View) {
+        appModel.restartGame()
+    }
+
+    private fun onTetrisViewTouch(view: View, event: MotionEvent): Boolean {
+        if (appModel.isGameOver() || appModel.isGameAwaitingStart()) {
+            appModel.startGame()
+            tetrisView.setGameCommandWithDelay(AppModel.Motions.DOWN)
+        } else if (appModel.isGameActive()) {
+            when (resolveTouchDirection(view, event)) {
+                0 -> moveTetromino(AppModel.Motions.LEFT)
+                1 -> moveTetromino(AppModel.Motions.ROTATE)
+                2 -> moveTetromino(AppModel.Motions.DOWN)
+                3 -> moveTetromino(AppModel.Motions.RIGHT)
+            }
+        }
+        return true
+    }
+
+    private fun resolveTouchDirection(view: View, event: MotionEvent): Int {
+        val x = event.x / view.width
+        val y = event.y / view.height
+        val direction: Int
+        direction = if (y>x) {
+            if (x > y-1) 2 else 0
+        } else {
+            if (x > y-1) 3 else 1
+        }
+        return direction
+    }
+
+    private fun moveTetromino(motion: AppModel.Motions) {
+        if (appModel.isGameActive()) {
+            tetrisView.setGameCommand(motion)
+        }
+    }
 }
